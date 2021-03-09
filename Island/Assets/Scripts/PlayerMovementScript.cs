a8b3d57df1741b2f66e39bbcd8e6e8779e88e673
Victor <44725192+Victronic@users.noreply.github.com>
2021-03-09 13:54:38 +0200

    Base mechanic for Prototype

---
+++ Island/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groudDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    Vector3 velocity;
+    bool isGorunded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGorunded = Physics.CheckSphere(groundCheck.position, groudDistance, groundMask);
+
+        if(isGorunded && velocity.y<0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward *z;
+
+        controller.Move(move * speed* Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGorunded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
