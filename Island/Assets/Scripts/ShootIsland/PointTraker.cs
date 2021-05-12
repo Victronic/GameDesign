b4ff22222db66cdcff1ac37193167d8fd2500ca8
Victor <44725192+Victronic@users.noreply.github.com>
2021-05-12 13:27:10 +0300

    ShootIsland Basic

---
+++ Island/Assets/Scripts/ShootIsland/PointTraker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointTraker : MonoBehaviour
+{
+    [SerializeField] GameObject impactPrefab;
+    public int value = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        ScoreManager.instance.score += value;
+        GameObject _canvas = Instantiate(impactPrefab, other.transform.position, other.transform.rotation);
+        _canvas.GetComponentInChildren<RectTransform>().position = Camera.main.WorldToScreenPoint(other.transform.position);
+        _canvas.GetComponentInChildren<Text>().text = value.ToString();
+        Destroy(_canvas, 2);
+    }
+}
---
+++ Island/Assets/Scripts/ShootIsland/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score = 0;
+    public static ScoreManager instance;
+    [SerializeField] Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Points: " + score.ToString();
+    }
+}
