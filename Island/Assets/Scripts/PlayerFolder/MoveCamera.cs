15b21f6fc85d032a749d788a8829f11fb646fc09
Victor <44725192+Victronic@users.noreply.github.com>
2021-05-17 16:42:50 +0300

    New improved Player

---
+++ Island/Assets/Scripts/PlayerFolder/MoveCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField] Transform cameraPositon;
+
+    void Update()
+    {
+        transform.position = cameraPositon.position;   
+    }
+}
